bcb79c30e0a383dc8c9105d9396be5a3c2777aa4
William <william@William.local>
2019-07-08 10:58:31 -0400

    Content block functionality added

--- js/main.js
+++ js/main.js
@@ -5,10 +5,17 @@ $(document).ready(function() {
     var $activeNavItem = $('.active-nav-item');
     var $selfPhoto = $('.self-photo');
     var $contentContainer = $('.content-container');
+    var $contentBlock = $('.content-block');
     var $defaultBg = '#c3d7ea';
 
     $navItem.on('click', function(e) {
-        console.log(e.target)
+        var classList = $(this).attr('class').split(/\s+/);
+        var selectedNav;
+        $.each(classList, function(index, item) {
+            if (item !== 'nav-item') {
+                selectedNav = item;
+            }
+        });
         $activeNavItem.text('')
         var shapeBgColor = $(this).find('.svg-background').css('fill');
         $hr.addClass('slide-hr-left');
@@ -17,8 +24,9 @@ $(document).ready(function() {
         setTimeout(function() {
             $activeNavItem.css('color', shapeBgColor).addClass('is-active');
             $contentContainer.css('background-color', shapeBgColor);
+            $contentContainer.find(`[data-name='${selectedNav}']`).addClass('is-active')
             $selfPhoto.addClass('back').children().eq(0).attr('src', 'assets/left-arrow.svg');
-         }, 1000);
+         }, 500);
     })
 
     $selfPhoto.on('click', function() {
@@ -29,15 +37,13 @@ $(document).ready(function() {
             $selfPhoto.removeClass('back');
             setTimeout(function() {
                 $hr.addClass('slide-hr-right');
-             }, 500);
+             }, 250);
             setTimeout(function() {
+               $contentBlock.removeClass('is-active');
                $hr.removeClass('slide-hr-right slide-hr-left');
                $navCurtain.removeClass('curtain-down curtain-up')
                $contentContainer.css('background-color', $defaultBg);
-            }, 1000);
+            }, 500);
         }
-        // else {
-        //     $(this).children().eq(0).css('border', '0')
-        // }
     })
 })
